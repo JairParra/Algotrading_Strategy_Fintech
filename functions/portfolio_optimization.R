eafb70acb318280c6d213cf0a731d1b436615b2b
Hair Parra <jair.parra@outlook.com>
2023-10-12 21:32:53 -0400

    portfolio optimization updates, bug fixes, merge logic

--- functions/portfolio_optimization.R
+++ functions/portfolio_optimization.R
@@ -6,17 +6,12 @@
 # @author: Kriti
 ################################################################################
 
-################
-### 0. Setup ###
-################
-
-
 #############################################
 ### 1. Data Manipulation for Optimization ###
 #############################################
 
 
-f_extract_ret_fore <- function(data, column_name) {
+f_extract_ret_fore <- function(data, column_name = "best_shifted_arima") {
   ## Function to extract the xts object for forecasted returns from a list of xts objects
   
   require("xts")
@@ -38,7 +33,7 @@ f_extract_ret_fore <- function(data, column_name) {
 }
 
 
-f_extract_vol_fore <- function(data, column_name) {
+f_extract_vol_fore <- function(data, returns_fore_index,  column_name ="vol_forecast") {
   ## Function to extract the xts object for forecasted volatility from a list of xts objects
   
   require("xts")
@@ -64,16 +59,16 @@ f_extract_vol_fore <- function(data, column_name) {
 ### 2. Mean and Covariance  ###
 ############################### 
 
-f_cov_matrix <- function(volat_matrix, tau) {
+f_cov_matrix <- function(volat_matrix) {
   ## Function to compute the covariance matrix based on filtered volatility
   
   require("Matrix")
 
-  # Filter the volatility matrix to include only the rows where month_index equals tau
-  filtered_volat <- volat_matrix[volat_matrix$month_index == tau, 1:(ncol(volat_matrix) - 1)]
+  # remove the month_index column
+  volat_matrix$month_index <- NULL
   
   # Compute the covariance matrix
-  cov_matrix <- crossprod(filtered_volat[1,], filtered_volat[1,]) # this will be tau + 1 if the features are against the correct dates
+  cov_matrix <- crossprod(volat_matrix[1,], volat_matrix[1,]) # this will be tau + 1 if the features are against the correct dates
   
   # Ensure the covariance matrix is positive definite
   cov_matrix <- nearPD(cov_matrix)
@@ -85,14 +80,17 @@ f_cov_matrix <- function(volat_matrix, tau) {
 }
 
 # Function to compute the mean return for portfolio optimization
-f_mean_ret <- function(return_matrix, tau) {
+f_mean_ret <- function(return_matrix) {
   
   require("Matrix")
   
-  # Filter the return matrix to include only rows where month_index equals tau and calculate the mean for each column
-  mean_filt_returns <- apply(return_matrix[return_matrix$month_index == tau, 1:(ncol(return_matrix) - 1)], 2, mean)
+  # remove the extra column 
+  return_matrix$month_index <- NULL
+  
+  # calculate the mean by column 
+  mean_returns <- apply(return_matrix, 2, mean)
 
-  return(mean_filt_returns)
+  return(mean_returns)
 }
 
 ##################################
@@ -109,22 +107,37 @@ f_optimize_portfolio <- function(top_sector_stocks){
   require("PerformanceAnalytics")
   
   # Get the data frame for returns
-  returns_fore <- f_extract_ret_fore(top_sector_stocks, "best_shifted_arima")
+  returns_fore <- f_extract_ret_fore(top_sector_stocks, column_name = "best_shifted_arima")
   
   # to check if there are any NA
   if(sum(is.na(returns_fore))){
     returns_fore <- na.omit(returns_fore)
   } 
   
-  # Get the data frame for volatility forecast
-  volatility_fore <- f_extract_vol_fore(top_sector_stocks, "vol_forecast")
-  sum(is.na(volatility_fore)) # to check if there are any NA
+  # obtain indexes of original object 
+  returns_fore_index <- index(returns_fore)
   
-  # Calculate covariance matrix 
-  cov_matrix <- f_cov_matrix(volatility_fore, tau)
+  # Get the data frame for volatility forecast
+  volatility_fore <- f_extract_vol_fore(top_sector_stocks, column_name =  "vol_forecast")
+  
+  # keep only rows that didn't have NAs
+  volatility_fore <- volatility_fore[returns_fore_index] 
+  
+  # verify that there are no NAs left 
+  if(sum(is.na(returns_fore)) > 0 || sum(is.na(volatility_fore)) >0){
+    stop("(f_optimize_portfolio) --> NAs found that couldn't be removed. ")
+  }
   
   # Minimum returns for portfolio optimization 
-  mean_returns <- f_mean_ret(returns_fore, tau)
+  mean_returns <- f_mean_ret(returns_fore)
+  
+  # Calculate covariance matrix 
+  cov_matrix <- f_cov_matrix(volatility_fore)
+  
+  # ensure dimensions are compatible 
+  if(length(mean_returns) != dim(cov_matrix)[1]){
+    stop("(f_optimize_portfolio) --> Incompatible dimensions of mean_returns and cov_matrix")
+  }
   
   # Perform min-variance portfolio optimization 
   output <- quadprog::solve.QP(Dmat = cov_matrix, # covariance matrix to be minimized
